7b6a94a762dd12d937523461aadc77aafd26bf2b
KALIMI <karenmirakyan@gmail.com>
2022-02-06 00:29:25 +0400

    signals demo added

--- users/models.py
+++ users/models.py
@@ -21,7 +21,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -33,3 +33,4 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
