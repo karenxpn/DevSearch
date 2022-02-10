b5423748aafbf7cc202ee750c74058f4ff0c6600
KALIMI <karenmirakyan@gmail.com>
2022-02-10 18:43:41 +0400

    edit profile added

--- users/signals.py
+++ users/signals.py
@@ -18,11 +18,21 @@ def create_profile(sender, instance, created, **kwargs):
 
 
 # @receiver(post_delete, sender=Profile)
-def profile_deleted(sender, instance, **kwargs):
+def delete_profile(sender, instance, **kwargs):
     user = instance.user
     user.delete()
     print('Deleting user...')
 
 
+def update_user(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    if not created:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
 post_save.connect(create_profile, sender=User)
-post_delete.connect(profile_deleted, sender=Profile)
+post_save.connect(update_user, sender=Profile)
+post_delete.connect(delete_profile, sender=Profile)
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 
 def login_user(request):
@@ -55,7 +55,7 @@ def register_user(request):
             messages.success(request, 'Your account was successfully created!')
 
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
 
         else:
             messages.error(request, 'An error has occurred during registration')
@@ -93,3 +93,17 @@ def user_account(request):
         'projects': projects
     }
     return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def edit_account(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
