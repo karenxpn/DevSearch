e79af87ea87c278d3e4d65661595264ecffe3744
KALIMI <karenmirakyan@gmail.com>
2022-02-20 23:37:13 +0400

    basic requests added with serialization

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_routes)
+    path('', views.get_routes),
+    path('projects/', views.get_projects),
+    path('project/<str:pk>/', views.get_project)
 ]
