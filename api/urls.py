f4b0d8c1cebfaeb5571fa6fde3388fbdc5c078a9
KALIMI <karenmirakyan@gmail.com>
2022-02-21 15:20:59 +0400

    worked on jwt

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('', views.get_routes),
     path('projects/', views.get_projects),
-    path('project/<str:pk>/', views.get_project)
+    path('project/<str:pk>/', views.get_project),
+
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
